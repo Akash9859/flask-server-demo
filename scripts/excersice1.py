018789d35ac1cc516cccb113609f97f9dd3ee37b
Akash <pakash101192@gmail.com>
2022-03-12 11:09:12 +0530

    ready flask

--- scripts/excersice1.py
+++ scripts/excersice1.py
@@ -2,7 +2,7 @@
 from sys import exc_info
 from flask import request
 import json
-import requestes
+import requests
 def init(configs):
     global configs_dict
     configs_dict = configs
@@ -10,11 +10,13 @@ def init(configs):
 def exchange_rate(request_json):
     print(request_json)
     try:
-        print(1)
+        print(request_json['url'])
         response = requests.get(request_json['url'])
-        data = json.loads(response.text)
-        print(data)
+        data = response.text
+        # print(data)
     except Exception as exe:
         a,b,c =exc_info()
         print('error in line no::',c.tb_lineno)
+        print(a)
+        print(b)
     return 'executed'
